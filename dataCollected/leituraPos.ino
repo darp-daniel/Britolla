166b03d729155ecca6e4c6feeefe883496c4edaa
lab <lab@PC-PLC-06.unb.br>
2025-06-26 17:13:12 -0300

    Leitura x e y angulo

---
+++ dataCollected/leituraPos.ino
@@ -0,0 +1,33 @@
+float angle;
+float volts;
+#define pot1 A0;
+#define pot2 A1;
+int angMax = 270;
+
+void setup(){
+  Serial.begin(115200);
+}
+
+void loop(){
+    
+}
+
+float calcularAng(int pin){
+    unsigned long leitura = analogRead(pin);
+    prevAngulo = angMax * leitura*(4.9/1023);
+    angulo = angMax - prevAngulo;
+    if(leitura > 511.5){
+        return 360-angulo
+    }
+    return angulo;
+}
+float calcularPosX(int pin1, int pin2){
+    float ang1 = calcularAng(pin1);
+    float ang2 = calcularAng(pin2);
+    return 140*(cos(ang1) + cos(ang1+ang2));
+}
+float calcularPosX(int pin1, int pin2){
+    float ang1 = calcularAng(pin1);
+    float ang2 = calcularAng(pin2);
+    return 140*(sin(ang1) + sin(ang1+ang2));
+}
