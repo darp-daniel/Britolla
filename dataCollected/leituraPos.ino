75e77b15539c9a8c2554e27c6c9facc48ec9cc1e
lab <lab@PC-PLC-06.unb.br>
2025-06-27 17:28:41 -0300

    Fixed and test

--- dataCollected/leituraPos.ino
+++ dataCollected/leituraPos.ino
@@ -9,7 +9,13 @@ void setup(){
 }
 
 void loop(){
-    
+    float l1 = analogRead(pot1);
+    float l2 = analogRead(pot2);
+    float x = calcularPosX(pot1, pot2);
+    float y = calcularPosY(pot1, pot2);
+    Serial.print();
+    Serial.print(",");
+    Serial.println();
 }
 
 float calcularAng(int pin){
@@ -23,7 +29,7 @@ float calcularPosX(int pin1, int pin2){
     float ang2 = calcularAng(pin2);
     return 140*(cos(ang1) + cos(ang1+ang2));
 }
-float calcularPosX(int pin1, int pin2){
+float calcularPosY(int pin1, int pin2){
     float ang1 = calcularAng(pin1);
     float ang2 = calcularAng(pin2);
     return 140*(sin(ang1) + sin(ang1+ang2));
