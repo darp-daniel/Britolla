fc3b8f10f3826f8b65ec2a9163a7ac7fbdfabca0
Daniel Alves <flaniel.arp@gmail.com>
2025-10-16 10:22:39 -0300

    Update coleta de dados do potenciometro

--- dataCollected/potenciometro/potenciometro.ino
+++ dataCollected/potenciometro/potenciometro.ino
@@ -9,7 +9,7 @@ Pot pot2(A1);
 float ultimoX = -999.0;
 float ultimoY = -999.0;
 
-int L = 100;
+int L = 14;
 
 // (Opcional, mas recomendado) Define uma pequena tolerância.
 // O valor só será impresso se a mudança for maior que este valor.
@@ -33,11 +33,11 @@ void loop() {
   float rad1 = pot1.angulo(ang1);
   float rad2 = pot2.angulo(ang2);
 
-  float y = L * (cos(rad1) + cos(rad1 + rad2));
-  float x = L * (sin(rad1) + sin(rad1 + rad2));
+  float x = L * (cos(rad1) + cos(rad1 + rad2));
+  float y = L * (sin(rad1) + sin(rad1 + rad2));
 
-  float yH = L * (cos(rad1));
-  float xH = L * (sin(rad1));
+  //float yH = L * (cos(rad1));
+  //float xH = L * (sin(rad1));
 
   // 2. A CONDIÇÃO é aplicada aqui, ANTES de imprimir.
   // Comparamos o valor recém-calculado de 'x' e 'y' com o último que foi impresso.
@@ -47,11 +47,11 @@ void loop() {
     // 3. Se a mudança for maior que a tolerância, imprimimos.
     Serial.print(x);
     Serial.print(",");
-    Serial.print(y);
-    Serial.print(",");
-    Serial.print(xH);
-    Serial.print(",");
-    Serial.println(yH);
+    Serial.println(y);
+    //Serial.print(",");
+    //Serial.print(xH);
+    //Serial.print(",");
+    //Serial.println(yH);
 
     // 4. E SÓ ENTÃO, atualizamos os últimos valores conhecidos.
     ultimoX = x;
